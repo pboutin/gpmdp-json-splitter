16aa05bda1b2e108fd57a964a5559bd3d9a4275b
Pascal Boutin <contact@pboutin.net>
2017-12-16 00:58:33 -0500

    Clear the file after being idle for 10sec

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const https = require('https');
 
 const GPMDP_JSON = process.env.GPMDP_JSON;
 const OUTPUT_DIR = './';
+const OUTPUT_FILE = `${OUTPUT_DIR}/current_song.txt`;
 
 if (!GPMDP_JSON) {
   console.error(`You must declare "GPMDP_JSON" in your env.`);
@@ -12,9 +13,14 @@ if (!GPMDP_JSON) {
 
 jsonFilePath = path.normalize(GPMDP_JSON);
 
+let timeoutRef;
+
 fs.watchFile(jsonFilePath, function() {
   const {song: {title, artist}} = JSON.parse(fs.readFileSync(jsonFilePath));
   const output = `${title} - ${artist}`
   console.log(`Updating : ${output}`);
-  fs.writeFileSync(`${OUTPUT_DIR}/current_song.txt`, output);
+  fs.writeFileSync(OUTPUT_FILE, output);
+
+  clearTimeout(timeoutRef);
+  timeoutRef = setTimeout(() => fs.writeFileSync(OUTPUT_FILE, ''), 10000);
 });
